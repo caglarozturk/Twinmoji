823a4779938fa7a525f099c29fd298021485a016
Çağlar ÖZTÜRK <ozturk_caglar@icloud.com>
2026-02-25 16:52:24 +0300

    Base version

---
+++ Twinmoji/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Twinmoji
+//
+//  Created by Çağlar ÖZTÜRK on 8.12.2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Twinmoji/FinishView.swift
@@ -0,0 +1,18 @@
+//
+//  FinishView.swift
+//  Twinmoji
+//
+//  Created by Çağlar ÖZTÜRK on 11.12.2025.
+//
+
+import SwiftUI
+
+struct FinishView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinishView()
+}
